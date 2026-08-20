da5444249fbe549ba58fd456dc143f6d3993e8a7
Hugo <hjgamarra@gmail.com>
2026-08-20 14:01:03 -0300

    v1.0.8:fix oracle12

--- src/adapters/oracle12/Oracle12Adapter.js
+++ src/adapters/oracle12/Oracle12Adapter.js
@@ -3,5 +3,4 @@ import { Oracle12DDL } from './Oracle12DDL.js';
 import { Oracle12DML } from './Oracle12DML.js';
 
 export class Oracle12Adapter extends Oracle11Adapter {
-  constructor(options = {}) { super(options); this.ddl = new Oracle12DDL(this); this.dml = new Oracle12DML(this); }
 }
